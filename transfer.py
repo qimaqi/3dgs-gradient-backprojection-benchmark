8c9cea8d72fc76af827f3d4e89a94d340000192a
Jannick <jannick.schroer@gmail.com>
2024-12-12 00:44:23 +0100

    Added Batching of LSegNet

--- transfer.py
+++ transfer.py
@@ -1,3 +1,4 @@
+import math
 import os
 import time
 from typing import Literal
@@ -218,8 +219,7 @@ def test_proper_pruning(splats, splats_after_pruning):
     )
 
 
-def create_feature_field_lseg(splats):
-
+def create_feature_field_lseg(splats, batch_count = 1):
     net = LSegNet(
         backbone="clip_vitl16_384",
         features=256,
@@ -259,86 +259,88 @@ def create_feature_field_lseg(splats):
     colors_feats_0 = torch.zeros(colors.shape[0], 3, device=colors.device)
     colors_feats_0.requires_grad = True
 
-    for image in tqdm(
-        sorted(colmap_project.images.values(), key=lambda x: x.name),
-        desc="Feature backprojection",
+    images = sorted(colmap_project.images.values(), key=lambda x: x.name)
+    batch_size = math.ceil(len(images) / batch_count) if batch_count > 0 else 1
+
+    for batch_start in tqdm(
+            range(0, len(images), batch_size),
+            desc="Feature backprojection (batches)",
     ):
+        batch = images[batch_start:batch_start + batch_size]
+        for image in batch:
+            viewmat = get_viewmat_from_colmap_image(image)
 
-        image_name = image.name  # .split(".")[0] + ".jpg"
+            width = int(K[0, 2] * 2)
+            height = int(K[1, 2] * 2)
 
-        viewmat = get_viewmat_from_colmap_image(image)
+            with torch.no_grad():
+                output, _, meta = rasterization(
+                    means,
+                    quats,
+                    scales,
+                    opacities,
+                    colors_all,
+                    viewmat[None],
+                    K[None],
+                    width=width,
+                    height=height,
+                    sh_degree=3,
+                )
 
-        width = int(K[0, 2] * 2)
-        height = int(K[1, 2] * 2)
-        with torch.no_grad():
-            output, _, meta = rasterization(
+                output = torch.nn.functional.interpolate(
+                    output.permute(0, 3, 1, 2).cuda(),
+                    size=(480, 480),
+                    mode="bilinear",
+                )
+                feats = net.forward(output)
+                feats = torch.nn.functional.normalize(feats, dim=1)
+                feats = torch.nn.functional.interpolate(
+                    feats, size=(height, width), mode="bilinear"
+                )[0]
+                feats = feats.permute(1, 2, 0)
+
+            output_for_grad, _, meta = rasterization(
                 means,
                 quats,
                 scales,
                 opacities,
-                colors_all,
+                colors_feats,
                 viewmat[None],
                 K[None],
                 width=width,
                 height=height,
-                sh_degree=3,
             )
 
-            output = torch.nn.functional.interpolate(
-                output.permute(0, 3, 1, 2).cuda(),
-                size=(480, 480),
-                mode="bilinear",
+            target = (output_for_grad[0] * feats).sum()
+
+            target.backward()
+
+            colors_feats_copy = colors_feats.grad.clone()
+
+            colors_feats.grad.zero_()
+
+            output_for_grad, _, meta = rasterization(
+                means,
+                quats,
+                scales,
+                opacities,
+                colors_feats_0,
+                viewmat[None],
+                K[None],
+                width=width,
+                height=height,
             )
-            feats = net.forward(output)
-            feats = torch.nn.functional.normalize(feats, dim=1)
-            feats = torch.nn.functional.interpolate(
-                feats, size=(height, width), mode="bilinear"
-            )[0]
-            feats = feats.permute(1, 2, 0)
 
-        output_for_grad, _, meta = rasterization(
-            means,
-            quats,
-            scales,
-            opacities,
-            colors_feats,
-            viewmat[None],
-            K[None],
-            width=width,
-            height=height,
-        )
+            target_0 = (output_for_grad[0]).sum()
 
-        target = (output_for_grad[0] * feats).sum()
+            target_0.backward()
 
-        target.backward()
-
-        colors_feats_copy = colors_feats.grad.clone()
-
-        colors_feats.grad.zero_()
-
-        output_for_grad, _, meta = rasterization(
-            means,
-            quats,
-            scales,
-            opacities,
-            colors_feats_0,
-            viewmat[None],
-            K[None],
-            width=width,
-            height=height,
-        )
-
-        target_0 = (output_for_grad[0]).sum()
-
-        target_0.backward()
-
-        gaussian_features += colors_feats_copy
-        gaussian_denoms += colors_feats_0.grad[:, 0]
-        colors_feats_0.grad.zero_()
+            gaussian_features += colors_feats_copy
+            gaussian_denoms += colors_feats_0.grad[:, 0]
+            colors_feats_0.grad.zero_()
     gaussian_features = gaussian_features / gaussian_denoms[..., None]
     gaussian_features = gaussian_features / gaussian_features.norm(dim=-1, keepdim=True)
     # Replace nan values with 0
-    # print("NaN features", torch.isnan(gaussian_features).sum())
     gaussian_features[torch.isnan(gaussian_features)] = 0
     t2 = time.time()
     print("Time taken for feature backprojection", t2 - t1)
@@ -353,6 +355,8 @@ def main(
         "inria", "gsplat"
     ] = "gsplat",  # Original or GSplat for checkpoints
     data_factor: int = 4,
+    feature_field_batch_count: int = 1,  # Number of batches to process for feature field
+    run_feature_field_on_cpu: bool = False,  # Run feature field on CPU
 ):
 
     if not torch.cuda.is_available():
@@ -367,7 +371,7 @@ def main(
     splats_optimized = prune_by_gradients(splats)
     test_proper_pruning(splats, splats_optimized)
     splats = splats_optimized
-    features = create_feature_field_lseg(splats)
+    features = create_feature_field_lseg(splats, feature_field_batch_count, run_feature_field_on_cpu)
     torch.save(features, f"{results_dir}/features_lseg.pt")
 
 
